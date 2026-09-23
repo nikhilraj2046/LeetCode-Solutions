ff2599018b5a0b13d449b71331e128b5547026ab
Nikhil Raj Sinha <nikhilraj2046@gmail.com>
2026-09-24 00:19:43 +0530

    Time: 106 ms (5.72%), Space: 28 MB (95.79%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& arr) {
+        int n=arr.size();
+        vector<int>ans(n,-1);
+        //using brute force
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<i+n;j++){
+                int idx=j%n;
+                if(arr[idx]>arr[i]){
+                    ans[i]=arr[idx];
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
