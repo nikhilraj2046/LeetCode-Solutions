7f2d44512b9509ed50fd772b7173bab3459d3274
Nikhil Raj Sinha <nikhilraj2046@gmail.com>
2026-09-24 00:59:10 +0530

    Time: 2 ms (79.02%), Space: 28.3 MB (82.68%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& arr) {
+        // int n=arr.size();
+        // vector<int>ans(n,-1);
+        // //using brute force
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<i+n;j++){
+        //         int idx=j%n; // circular array
+        //         if(arr[idx]>arr[i]){
+        //             ans[i]=arr[idx];
+        //             break;
+        //         }
+        //     }
+        // }
+        // return ans;
+
+        //optimised approach using monotonic stck
         int n=arr.size();
-        vector<int>ans(n,-1);
-        //using brute force
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<i+n;j++){
-                int idx=j%n;
-                if(arr[idx]>arr[i]){
-                    ans[i]=arr[idx];
-                    break;
-                }
+        stack<int>st;
+        vector<int>nge(n,-1);
+       
+        for(int i=2*n-1;i>=0;i--){
+            while(!st.empty() && st.top()<=arr[i%n]){
+                st.pop();
             }
+            if(i<n){
+                nge[i]=st.empty()?-1:st.top();
+            }
+            st.push(arr[i%n]);
         }
-        return ans;
+        return nge;
     }
 };
